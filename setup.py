60f7f00725a308b5364ede88f12cd59f7cca9057
Adwait <adwaitmahajan01@gmail.com>
2025-07-08 01:35:07 -0600

    Project Structure Ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Adwait Mahajan",
+    author_email="adwaitmahajan01@gmail.com",
+    packages=find_packages()
+)
